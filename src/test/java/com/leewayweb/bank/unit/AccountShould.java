a27b86f25721b86a2f9fe06c015967c361ef1620
Mauro Chojrin <mauro.chojrin@leewayweb.com>
2025-09-24 17:29:07 +0200

    More generic test

--- src/test/java/com/leewayweb/bank/unit/AccountShould.java
+++ src/test/java/com/leewayweb/bank/unit/AccountShould.java
@@ -4,28 +4,40 @@ import com.leewayweb.bank.Account;
 import com.leewayweb.bank.Transaction;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class AccountShould {
-    private static final Transaction A_TRANSACTION = new Transaction(90, "07/04/2023");
     private Account account;
 
+    public static Stream<Arguments> transactionProvider() {
+        return Stream.of(
+                Arguments.of(
+                        new Transaction(90, "07/04/2023"),
+                        new Transaction(-10, "08/24/2021")
+                )
+        );
+    }
+
     @BeforeEach
-    public void setUp()
-    {
+    public void setUp() {
         this.account = new Account();
     }
 
-    @Test
-    public void keepTrackOfItsTransactions() {
-        account.addTransaction(A_TRANSACTION);
+    @ParameterizedTest
+    @MethodSource("transactionProvider")
+    public void keepTrackOfItsTransactions(Transaction transaction) {
+        account.addTransaction(transaction);
         List<Transaction> transactions = account.transactions();
 
         assertEquals(1, transactions.size());
-        assertEquals(A_TRANSACTION, transactions.getFirst());
+        assertEquals(transaction, transactions.getFirst());
     }
 
     @Test
