77eb73bd45fb052c4e55870a4cfcfffbb2a8c69d
Mauro Chojrin <mauro.chojrin@leewayweb.com>
2025-09-24 10:55:35 +0200

    Test and basic supporting classes written

---
+++ src/main/java/com/leewayweb/bank/Console.java
@@ -0,0 +1,7 @@
+package com.leewayweb.bank;
+
+public class Console {
+    public void printLine(String s) {
+        throw new UnsupportedOperationException("printLine not yet implemented");
+    }
+}
--- src/test/java/com/leewayweb/bank/feature/PrintStatementFeature.java
+++ src/test/java/com/leewayweb/bank/feature/PrintStatementFeature.java
@@ -1,12 +1,40 @@
 package com.leewayweb.bank.feature;
 
+import com.leewayweb.bank.AccountService;
+import com.leewayweb.bank.Console;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.verify;
 
+@ExtendWith(MockitoExtension.class)
 public class PrintStatementFeature {
+    @Mock Console console;
+    private AccountService accountService;
+
+    @BeforeEach
+    void setUp() {
+        accountService = new AccountService();
+    }
+
     @Test
-    public void shouldDoSomething() {
+    public void print_statement_containing_all_transactions() {
+
+        accountService.deposit(1000);
+        accountService.withdraw(100);
+        accountService.deposit(500);
+
+        accountService.printStatement();
+
+        verify(console).printLine("DATE | AMOUNT | BALANCE");
+        verify(console).printLine("10/04/2020 | 500.00 | 1400.00");
+        verify(console).printLine("02/04/2020 | -100.00 | 900.00");
+        verify(console).printLine("01/04/2020 | 1000.00 | 1000.00");
+
         assertTrue(true);
     }
 }
