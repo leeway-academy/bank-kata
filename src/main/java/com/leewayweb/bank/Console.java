e6ae2dddcb3db560702cc31fed92608a2e079b89
Mauro Chojrin <mauro.chojrin@leewayweb.com>
2025-09-24 17:12:26 +0200

    Completed console implementation

--- src/main/java/com/leewayweb/bank/Console.java
+++ src/main/java/com/leewayweb/bank/Console.java
@@ -2,6 +2,6 @@ package com.leewayweb.bank;
 
 public class Console {
     public void printLine(String s) {
-        throw new UnsupportedOperationException("printLine not yet implemented");
+        System.out.println(s);
     }
 }
