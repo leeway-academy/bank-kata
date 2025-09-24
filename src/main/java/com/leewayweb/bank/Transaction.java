f3f15ca54ed07456f1b63dcf72f33ead2dcc8a6f
Mauro Chojrin <mauro.chojrin@leewayweb.com>
2025-09-24 11:25:08 +0200

    Defined equality between transactions

--- src/main/java/com/leewayweb/bank/Transaction.java
+++ src/main/java/com/leewayweb/bank/Transaction.java
@@ -1,6 +1,19 @@
 package com.leewayweb.bank;
 
 public class Transaction {
+    private final int amount;
+
     public Transaction(int amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other instanceof Transaction && ((Transaction) other).amount == this.amount;
     }
 }
