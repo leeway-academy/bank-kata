25173050a82da649912d2fad3a7a652c5b2a7b04
Mauro Chojrin <mauro.chojrin@leewayweb.com>
2025-09-24 11:24:46 +0200

    Adding functionality to pass the test

--- src/main/java/com/leewayweb/bank/AccountService.java
+++ src/main/java/com/leewayweb/bank/AccountService.java
@@ -1,11 +1,14 @@
 package com.leewayweb.bank;
 
 public class AccountService {
+    private final Account account;
+
     public AccountService(Account account) {
+        this.account = account;
     }
 
     public void deposit(int amount) {
-
+        account.addTransaction(new Transaction(amount));
     }
 
     public void withdraw(int amount) {
