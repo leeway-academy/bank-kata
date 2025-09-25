2d2795b034625b149e65b6e5837f4f7e486c48bd
Mauro Chojrin <mauro.chojrin@leewayweb.com>
2025-09-25 08:33:04 +0200

    More expressive code

--- src/test/java/com/leewayweb/bank/unit/StatementPrinterShould.java
+++ src/test/java/com/leewayweb/bank/unit/StatementPrinterShould.java
@@ -3,7 +3,6 @@ package com.leewayweb.bank.unit;
 import com.leewayweb.bank.Console;
 import com.leewayweb.bank.StatementPrinter;
 import com.leewayweb.bank.Transaction;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -15,7 +14,7 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 import java.util.stream.Stream;
 
 @ExtendWith(MockitoExtension.class)
@@ -24,18 +23,22 @@ class StatementPrinterShould {
     private static final String HEADER = "DATE | AMOUNT | BALANCE";
     public static final String WITHDRAWAL_LINE = "22/12/1978 | -50 | 50";
     public static final String DEPOSIT_LINE = "22/12/1977 | 100 | 100";
+
+    public static final ArrayList<Transaction> NO_TRANSACTIONS = new ArrayList<>();
+    private static final Transaction A_DEPOSIT = new Transaction(100, "22/12/1977");
+    private static final Transaction A_WITHDRAWAL = new Transaction(-50, "22/12/1978");
+
     private @Mock Console console;
     @InjectMocks
     private StatementPrinter printer;
 
-    private static final Transaction A_DEPOSIT = new Transaction(100, "22/12/1977");
-    private static final Transaction A_WITHDRAWAL = new Transaction(-50, "22/12/1978");
-
     @ParameterizedTest
     @MethodSource("provider")
     public void printTransactionsInReverseChronologicalOrder(ArrayList<Transaction> transactions, ArrayList<String> lines) {
-        printer.print(transactions);
         InOrder inOrder = Mockito.inOrder(console);
+
+        printer.print(transactions);
+
         for (String line : lines) {
             inOrder.verify(console).printLine(line);
         }
@@ -44,21 +47,29 @@ class StatementPrinterShould {
     private static Stream<Arguments> provider() {
         return Stream.of(
                 Arguments.of(
-                        new ArrayList<Transaction>(),
-                        new ArrayList<>(List.of(HEADER))
+                        NO_TRANSACTIONS,
+                        lines(HEADER)
                 ),
                 Arguments.of(
-                        new ArrayList<>(List.of(A_DEPOSIT)),
-                        new ArrayList<>(List.of(HEADER, DEPOSIT_LINE))
+                        transactions(A_DEPOSIT),
+                        lines(HEADER, DEPOSIT_LINE)
                 ),
                 Arguments.of(
-                        new ArrayList<>(List.of(A_DEPOSIT, A_WITHDRAWAL)),
-                        new ArrayList<>(List.of(HEADER, WITHDRAWAL_LINE, DEPOSIT_LINE))
+                        transactions(A_DEPOSIT, A_WITHDRAWAL),
+                        lines(HEADER, WITHDRAWAL_LINE, DEPOSIT_LINE)
                 ),
                 Arguments.of(
-                        new ArrayList<>(List.of(A_WITHDRAWAL, A_DEPOSIT)),
-                        new ArrayList<>(List.of(HEADER, WITHDRAWAL_LINE, DEPOSIT_LINE))
+                        transactions(A_WITHDRAWAL, A_DEPOSIT),
+                        lines(HEADER, WITHDRAWAL_LINE, DEPOSIT_LINE)
                 )
         );
+    }
+
+    private static ArrayList<Transaction> transactions(Transaction... transactions) {
+        return new ArrayList<>(Arrays.asList(transactions));
+    }
+
+    private static ArrayList<String> lines(String... strings) {
+        return new ArrayList<>(Arrays.asList(strings));
     }
 }
